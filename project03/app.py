0e5ec2978a242cd1ea8cad3b5f8da5e01d23d360
taewan <ntw2100@gmail.com>
2022-05-02 22:29:51 +0900

    project3 맛집 지도

--- project03/app.py
+++ project03/app.py
@@ -4,7 +4,6 @@ from pymongo import MongoClient
 
 app = Flask(__name__)
 
-# client = MongoClient('localhost', 27017)
 client = MongoClient('13.209.18.100', 27017, username="test", password="test")
 db = client.dbsparta_plus_week3
 
@@ -13,10 +12,27 @@ db = client.dbsparta_plus_week3
 def main():
     return render_template("index.html")
 
-@app.route('/map')
-def test_map():
-    return render_template("prac_map.html")
+@app.route('/matjip', methods=["GET"])
+def get_matjip():
+    # 맛집 목록을 반환하는 API
+    matjip_list = list(db.matjips.find({}, {'_id': False}))
+    # matjip_list 라는 키 값에 맛집 목록을 담아 클라이언트에게 반환합니다.
+    # print(matjip_list)
+    return jsonify({'result': 'success', 'matjip_list': matjip_list})
 
+@app.route('/like_matjip', methods=["POST"])
+def like_matjip():
+    title = request.form['title_give']
+    address = request.form['address_give']
+    action = request.form['action_give']
+    print(title, action)
+
+    if action == 'unlike':
+        db.matjips.update_one({'title': title, 'address': address}, {'$unset': {'like': False}})
+    else:
+        db.matjips.update_one({'title': title, 'address': address}, {'$set': {'like': True}})
+
+    return jsonify({'result': 'success'})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
---
+++ project03/scraping.py
@@ -0,0 +1,69 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import time
+from selenium.common.exceptions import NoSuchElementException
+from pymongo import MongoClient
+import requests
+
+client = MongoClient('13.209.18.100', 27017, username="test", password="test")
+db = client.dbsparta_plus_week3
+
+driver = webdriver.Chrome('./chromedriver')
+
+url = "http://matstar.sbs.co.kr/location.html"
+
+driver.get(url)
+time.sleep(5)
+#더 보기 버튼의 선택자로 버튼 클릭
+# Please use find_element(by=By.CSS_SELECTOR, value=css_selector) instead
+for i in range(10):
+    try:
+        btn_more = driver.find_element_by_css_selector("#foodstar-front-location-curation-more-self > div > button")
+        btn_more.click()
+        time.sleep(5)
+    except NoSuchElementException:
+        break
+
+req = driver.page_source
+driver.quit()
+
+soup = BeautifulSoup(req, 'html.parser')
+
+# 각 식당에 해당하는 카드 선택
+places = soup.select("ul.restaurant_list > div > div > li > div > a")
+print(len(places))
+
+# 식당 이름, 주소, 카테고리, 출연 프로그램과 회차 정보
+for place in places:
+    title = place.select_one("strong.box_module_title").text
+    address = place.select_one("div.box_module_cont > div > div > div.mil_inner_spot > span.il_text").text
+    category = place.select_one("div.box_module_cont > div > div > div.mil_inner_kind > span.il_text").text
+    show, episode = place.select_one("div.box_module_cont > div > div > div.mil_inner_tv > span.il_text").text.rsplit(" ", 1)
+    print(title, address, category, show, episode)
+    #지오코딩 연결하기
+    headers = {
+        "X-NCP-APIGW-API-KEY-ID": "98hquhx0x7",
+        "X-NCP-APIGW-API-KEY": "gOPbsaUYxHdcFsmFsAQ2IW3cMzPQYM7EX4Rjgp8q"
+    }
+    r = requests.get(f"https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode?query={address}", headers=headers)
+    response = r.json()
+    # 결과 출력하기
+    if response["status"] == "OK":
+        if len(response["addresses"]) > 0:
+            x = float(response["addresses"][0]["x"])
+            y = float(response["addresses"][0]["y"])
+            print(title, address, category, show, episode, x, y)
+            #디비에 저장하기
+            doc = {
+                "title": title,
+                "address": address,
+                "category": category,
+                "show": show,
+                "episode": episode,
+                "mapx": x,
+                "mapy": y}
+            db.matjips.insert_one(doc)
+
+
+        else:
+            print(title, "좌표를 찾지 못했습니다")
